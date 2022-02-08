02e0719b301e0676170228fd92f4e832249680ae
Andrii <kotsupyr.a@gmail.com>
2022-02-08 11:10:20 +0200

    add snake

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,53 +1,112 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Food from "../Food";
+import Snake from "../Snake";
+import useInterval from "../../hooks/useInterval";
 import styles from "./Grid.module.scss";
 
 export default function Grid() {
-  const [rows, setRows] = useState(10);
-  const [cols, setCols] = useState(10);
-  const [grid, setGrid] = useState([]);
-
-  function getRandomGrid() {
-    return {
-      row: Math.floor(Math.random() * rows),
-      col: Math.floor(Math.random() * cols),
-    };
+  const width = 10;
+  const height = 10;
+  let initialRows = [];
+  for (let i = 0; i < height; i++) {
+    initialRows.push([]);
+    for (let k = 0; k < width; k++) {
+      initialRows[i].push("blank");
+    }
   }
 
-  useEffect(() => {
-    const tempGrid = [];
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-        const food = getRandomGrid();
-        const isFood = food.row === row && food.col === col;
-        tempGrid.push({
-          row,
-          col,
-          isFood,
-        });
-      }
-    }
-    setGrid(tempGrid);
-  }, []);
+  const randomPosition = () => {
+    const position = {
+      x: Math.floor(Math.random() * width),
+      y: Math.floor(Math.random() * height),
+    };
+    return position;
+  };
 
-  const gridItems = grid.map((grid) => {
-    if (grid.isFood) {
-      return (
-        <Food key={grid.row.toString() + "-" + grid.col.toString()}></Food>
-      );
-    } else {
-      return (
-        <div
-          key={grid.row.toString() + "-" + grid.col.toString()}
-          className={styles.gridItem}
-        ></div>
-      );
+  const [rows, setRows] = useState(initialRows);
+  const [snake, setSnake] = useState([
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+  ]);
+  const [direction, setDirection] = useState("right");
+  const [food, setFood] = useState(randomPosition);
+
+  const changeDirectionWithKeys = (e) => {
+    const { keyCode } = e;
+    switch (keyCode) {
+      case 37:
+        setDirection("left");
+        break;
+      case 38:
+        setDirection("top");
+        break;
+      case 39:
+        setDirection("right");
+        break;
+      case 40:
+        setDirection("bottom");
+        break;
+      default:
+        break;
     }
-  });
+  };
+
+  document.addEventListener("keydown", changeDirectionWithKeys, false);
+
+  function displaySnake() {
+    const newRows = initialRows;
+    snake.forEach((cell) => {
+      newRows[cell.x][cell.y] = "snake";
+    });
+    newRows[food.x][food.y] = "food";
+    setRows(newRows);
+  }
+
+  function moveSnake() {
+    const newSnake = [];
+    switch (direction) {
+      case "right":
+        newSnake.push({ x: snake[0].x, y: (snake[0].y + 1) % width });
+        break;
+      case "left":
+        newSnake.push({ x: snake[0].x, y: (snake[0].y - 1 + width) % width });
+        break;
+      case "top":
+        newSnake.push({ x: (snake[0].x - 1 + height) % height, y: snake[0].y });
+        break;
+      case "bottom":
+        newSnake.push({ x: (snake[0].x + 1) % height, y: snake[0].y });
+    }
+    snake.forEach((cell) => {
+      newSnake.push(cell);
+    });
+    if (snake[0].x === food.x && snake[0].y === food.y) {
+      setFood(randomPosition);
+    } else {
+      newSnake.pop();
+    }
+    setSnake(newSnake);
+    displaySnake();
+  }
+
+  useInterval(moveSnake, 200);
+
+  const displayRows = rows.map((row) =>
+    row.map((e) => {
+      switch (e) {
+        case "blank":
+          return <div className={styles.gridItem}></div>;
+        case "snake":
+          return <Snake />;
+        case "food":
+          return <Food />;
+      }
+    })
+  );
 
   return (
     <div className={styles.snakeContainer}>
-      <div className={styles.grid}>{gridItems}</div>
+      <div className={styles.grid}>{displayRows}</div>
     </div>
   );
 }
---
+++ src/components/Snake/Snake.jsx
@@ -0,0 +1,5 @@
+import styles from "./Snake.module.scss";
+
+export default function Snake() {
+  return <div className={styles.snake}></div>;
+}
