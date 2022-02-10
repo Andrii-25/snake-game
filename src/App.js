832b4e36f14f0df4412368a270f3d1f1679b0b5f
Andrii <kotsupyr.a@gmail.com>
2022-02-10 18:39:20 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import "./App.css";
-import Grid from "./components/GameField";
-import Header from "./components/Header";
-import LoginForm from "./components/LoginForm/LoginForm";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <LoginForm />
-      <Grid /> */}
       <MainPage />
     </div>
   );
