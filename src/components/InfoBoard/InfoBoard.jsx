126bfb1cbf9b13ac0a7125308ed7d2d64e560277
Andrii <kotsupyr.a@gmail.com>
2022-02-11 20:10:55 +0200

    update info board

--- src/components/InfoBoard/InfoBoard.jsx
+++ src/components/InfoBoard/InfoBoard.jsx
@@ -1,6 +1,8 @@
 import appleRed from "../../images/appleRed.png";
 import appleGreen from "../../images/appleGreen.png";
 import appleYellow from "../../images/appleYellow.png";
+import spaceKey from "../../images/spaceKey.png";
+import escKey from "../../images/escKey.png";
 import styles from "./InfoBoard.module.scss";
 
 export default function InfoBoard() {
@@ -50,6 +52,34 @@ export default function InfoBoard() {
           </td>
           <td className={styles.td}>
             <h2>10 points</h2>
+          </td>
+        </tr>
+
+        <tr>
+          <td className={styles.td}>
+            <h2>
+              <img src={spaceKey} alt="Space key" width={50} height={35} />
+            </h2>
+          </td>
+          <td className={styles.td}>
+            <h2>-</h2>
+          </td>
+          <td className={styles.td}>
+            <h2>set pause</h2>
+          </td>
+        </tr>
+
+        <tr>
+          <td className={styles.td}>
+            <h2>
+              <img src={escKey} alt="Esc key" width={50} height={35} />
+            </h2>
+          </td>
+          <td className={styles.td}>
+            <h2>-</h2>
+          </td>
+          <td className={styles.td}>
+            <h2>unset pause</h2>
           </td>
         </tr>
       </table>
