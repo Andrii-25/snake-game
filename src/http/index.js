dfb762b5f78ff888d55265ad79c7a2c177835da0
Andrii <kotsupyr.a@gmail.com>
2022-02-12 15:32:10 +0200

    update api url

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = process.env.API_URL || "http://localhost:8000/api";
+export const API_URL = process.env.REACT_APP_API_URL;
 
 const $api = axios.create({
   withCredentials: true,
